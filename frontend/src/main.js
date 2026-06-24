d9b46448c982cdbee5faa1d07e02aba7506ab1d6
parkshinyoung <mssy6292@naver.com>
2026-06-24 21:00:56 +0900

    style(frontend): align styles with design tokens

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/styles/tokens.css'
 import './assets/styles/base.css'
+import './assets/styles/bootstrap-overrides.css'
+import './assets/styles/components.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import App from './App.vue'
 import router from './router'
