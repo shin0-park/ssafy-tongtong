721fae0ff097a6cf88da1c09e72bc2710677c97f
parkshinyoung <mssy6292@naver.com>
2026-06-25 20:35:45 +0900

    changeMiniCardandFixture

--- frontend/src/components/cards/RelatedProgramMiniCard.vue
+++ frontend/src/components/cards/RelatedProgramMiniCard.vue
@@ -1,15 +1,26 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
   program: {
     type: Object,
     required: true,
   },
 })
+
+const titleText = computed(() => props.program.title || props.program.name || '관련 프로그램')
+const detailTo = computed(() => (props.program.id ? `/programs/${props.program.id}` : ''))
 </script>
 
 <template>
   <article class="mini-card">
-    <strong>{{ program.title || program.name || '관련 프로그램' }}</strong>
+    <strong>
+      <RouterLink v-if="detailTo" class="text-decoration-none" :to="detailTo">
+        {{ titleText }}
+      </RouterLink>
+      <span v-else>{{ titleText }}</span>
+    </strong>
     <p class="meta-text mb-0">{{ program.category_display || program.operation_status_display || '프로그램 정보' }}</p>
   </article>
 </template>
