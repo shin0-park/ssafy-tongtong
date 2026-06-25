2b54213976f8476e6b967950112134f41e442b48
parkshinyoung <mssy6292@naver.com>
2026-06-25 23:44:49 +0900

    changebuttonstyle

--- frontend/src/components/navigation/BackLink.vue
+++ frontend/src/components/navigation/BackLink.vue
@@ -14,7 +14,7 @@ defineProps({
 <template>
   <div class="page-back-row">
     <RouterLink class="page-back-button text-decoration-none" :to="to" :aria-label="label" :title="label">
-      <span aria-hidden="true">&lt;</span>
+      <span aria-hidden="true">‹</span>
       <span class="visually-hidden">{{ label }}</span>
     </RouterLink>
   </div>
